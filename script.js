2b00e754d75e381605917a16d0c11cabae1c0801
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-05-14 20:59:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
 // your code here
+	if(n<=1)return n;
+
+	let dp = [0,1];
+	for(let i=0; i<=num; i++){
+		let ans = dp[i-1]+dp[i-2];
+		dp.push(ans);
+	}
+	return dp[num];
 }
 
 module.exports = fibonacci;
